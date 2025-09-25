0929895439d0331b21e851cde4b374be03fb23b7
anewgd <ephremabraham8997@gmail.com>
2025-09-25 15:45:37 +0300

    token test

--- src/routes/authorize/+page.svelte
+++ src/routes/authorize/+page.svelte
@@ -1,17 +1,49 @@
 <script>
 	import { page } from '$app/state';
+	import { error } from '@sveltejs/kit';
 
     let { data } = $props();
 
     let message = $state("");
+    let errMsg = $state({});
+    let tokenLoadSuccessful = $state(false);
+    let tokenLoadFailed = $state(false);
+    let tokenResponse = $state({});
 
 
     const urlParams = new URLSearchParams(page.url.search);
 
-    let code = urlParams.get('code');
+
+
+
+    let code = urlParams.get('code') ?? "";
+
     let auth_state = urlParams.get('state'); 
+
+    const fetchToken = async () => {
+        try {
+            tokenResponse = await fetch("/token", {
+            body: JSON.stringify({code}),
+            })
+
+    
+            
+        } catch (e) {
+
+            errMsg = e instanceof Error ? e : String(e)
+        }
+    }
+     
+
 
     message = "Will send token request to " + data.config.token_url + " with code: " + code + " and state: " + auth_state;
 </script>
 
-<h1>{message}</h1>
+<div onload="{fetchToken}"></div>
+
+{#if tokenLoadSuccessful }
+    <div class="font-size-2xl m-4 flex justify-center bg-green-300 p-4 border-radius-xl">{tokenResponse}</div>
+{:else if tokenLoadFailed }
+    <div class="font-size-2xl m-4 flex justify-center bg-red-300 p-4 border-radius-xl">{errMsg}</div>
+{/if}
+
---
+++ src/routes/token/+server.js
@@ -0,0 +1,38 @@
+import { REDIRECT_URI } from "$env/static/private";
+import { config } from "$lib/config/config.js";
+import axios from "axios";
+
+
+let reqBody = {
+    grant_type: "code",
+    REDIRECT_URI: config.REDIRECT_URI,
+    code: ""
+}
+
+
+export async function POST({request}) {
+
+    let { code } = await request.json();
+    let url = config.TOKEN_URL;
+
+    let reqBody = {
+    grant_type: "code",
+    REDIRECT_URI: config.REDIRECT_URI,
+    code: code
+}
+    const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(reqBody),
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": 'Basic ' + Buffer.from(config.CLIENT_ID + ":" + config.CLIENT_SECRET).toString("base64"),
+        },
+        credentials: "include"
+
+    })
+
+    let resp = await response.json()
+
+    return resp
+
+}
