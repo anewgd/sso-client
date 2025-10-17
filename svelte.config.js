3962980ed0f56f9a71d1b19168107138da244e44
anewgd <ephremabraham8997@gmail.com>
2025-10-17 14:51:29 +0300

    updated csrf

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,11 @@
 import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = { kit: { adapter: adapter(), csrf: {
+    trustedOrigins: [
+        'https://ssoapiv2.automatrix.et',
+        'http://localhost:3001'
+    ]
+} } };
 
 export default config;
